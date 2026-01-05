a0872e6ca01cde487b6674efa250f3d78ccc0338
Pavel <lyubimov99@gmail.com>
2026-01-05 22:05:10 +0200

    add carousel beta and integrate into page

--- src/components/FirstPage/FirstPage.tsx
+++ src/components/FirstPage/FirstPage.tsx
@@ -1,13 +1,34 @@
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import "./firstPage.css";
+import { Carousel } from "../common/Carousel/Carousel";
 
 export default function FirstPage() {
   const navigate = useNavigate();
   const { t } = useTranslation();
+  const versionSelector =[
+    { id: '1', title: t("firstPage.titleCarousel"), img: '/public/assets/versionGame/bases-1-4.webp' },
+    { id: '2', title: t("firstPage.titleCarousel"), img: '/public/assets/versionGame/bases+players5-6.webp' },
+    { id: '3', title: t("firstPage.titleCarousel"), img: '/public/assets/versionGame/bases-1-4.webp' }
+  ]
 
+
+  const mapSlides = versionSelector.map(map => (
+    <div className="map-card" key={map.id}>
+      <img 
+        src={map.img} 
+        alt={map.title} 
+        style={{ width: '100%', borderRadius: '12px' }} 
+      />
+      <h3>{map.title}</h3>
+    </div>
+  ));
+  
   return (
     <div className="FirstPage">
+      <div className="Carousel">
+        <Carousel slides={mapSlides} />
+      </div>
       <div className="content">
         <button className="generate-btn">{t("firstPage.generateMap")}</button>
       </div>
---
+++ src/components/common/Carousel/Carousel.tsx
@@ -0,0 +1,28 @@
+import React, { type ReactNode } from 'react'
+import useEmblaCarousel from 'embla-carousel-react'
+import './carousel.css'
+
+interface CarouselProps {
+  slides: ReactNode[];
+  options?: {
+    loop?: boolean;
+    align?: 'start' | 'center' | 'end';
+  };
+}
+
+export const Carousel: React.FC<CarouselProps> = ({ slides, options = { loop: true } }) => {
+
+  const [emblaRef] = useEmblaCarousel(options)
+
+  return (
+    <div className="embla" ref={emblaRef}>
+      <div className="embla__container">
+        {slides.map((slide, index) => (
+          <div className="embla__slide" key={index}>
+            {slide}
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
