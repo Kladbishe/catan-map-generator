3bbcdf9cb2bb227069025be88b7e6d4f6c61f7e9
Pavel <lyubimov99@gmail.com>
2026-01-06 21:27:08 +0200

    Add game variant translations and update carousel styling

--- src/components/FirstPage/FirstPage.tsx
+++ src/components/FirstPage/FirstPage.tsx
@@ -7,20 +7,24 @@ export default function FirstPage() {
   const navigate = useNavigate();
   const { t } = useTranslation();
   const versionSelector =[
-    { id: '1', title: t("firstPage.titleCarousel"), img: '/public/assets/versionGame/bases-1-4.webp' },
-    { id: '2', title: t("firstPage.titleCarousel"), img: '/public/assets/versionGame/bases+players5-6.webp' },
-    { id: '3', title: t("firstPage.titleCarousel"), img: '/public/assets/versionGame/bases-1-4.webp' }
+    { id: '1', title: t("firstPage.games.basic.titleCarousel"),subtitle: t("firstPage.games.basic.subtitle"),description: t("firstPage.games.basic.description") ,img: '/public/assets/versionGame/bases-1-4.webp' },
+    { id: '2', title: t("firstPage.games.basicStandardExtension.titleCarousel"),subtitle: t("firstPage.games.basicStandardExtension.subtitle"),description: t("firstPage.games.basicStandardExtension.description"), img: '/public/assets/versionGame/bases+players5-6.webp' },
+    { id: '3', title: t("firstPage.games.seafarers.titleCarousel"),subtitle: t("firstPage.games.seafarers.subtitle"),description: t("firstPage.games.seafarers.description"), img: '/public/assets/versionGame/bases-1-4.webp' },
+    { id: '4', title: t("firstPage.games.seafarersExtension.titleCarousel"),subtitle: t("firstPage.games.seafarersExtension.subtitle"),description: t("firstPage.games.seafarersExtension.description"), img: '/public/assets/versionGame/bases-1-4.webp' }
+  
   ]
 
 
   const mapSlides = versionSelector.map(map => (
     <div className="map-card" key={map.id}>
-      <img 
-        src={map.img} 
-        alt={map.title} 
-        style={{ width: '100%', borderRadius: '12px' }} 
+      <img
+        src={map.img}
+        alt={map.title}
+        style={{ width: '85%', borderRadius: '12px' }}
       />
       <h3>{map.title}</h3>
+      <h5>{map.subtitle}</h5>
+      <p>{map.description}</p>
     </div>
   ));
   
